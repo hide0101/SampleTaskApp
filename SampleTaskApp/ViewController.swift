70ae45fae46db6daa06b467dc41aac40609245eb
hide0101 <hideakikomori0101@gmail.com>
2019-01-20 00:34:48 +0900

    Initial Commit

---
+++ SampleTaskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleTaskApp
+//
+//  Created by 小森英明 on 2019/01/20.
+//  Copyright © 2019 hide0101. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SampleTaskAppTests/SampleTaskAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleTaskAppTests.swift
+//  SampleTaskAppTests
+//
+//  Created by 小森英明 on 2019/01/20.
+//  Copyright © 2019 hide0101. All rights reserved.
+//
+
+import XCTest
+@testable import SampleTaskApp
+
+class SampleTaskAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
